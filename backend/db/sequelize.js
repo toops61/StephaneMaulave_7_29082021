8170af48f6d95612db1e125d3454384f65d80b3e
Toops61 <toops61@gmail.com>
2021-10-01 17:05:27 +0200

    fix sequelize BDD env

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -24,8 +24,8 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: 'localhost',
-        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
         logging: true
     }
 )
