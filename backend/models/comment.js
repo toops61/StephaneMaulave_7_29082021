d7f8737dea735bc64a5afec898f2acde844a12fe
Toops61 <toops61@gmail.com>
2021-10-12 06:00:07 +0200

    fix bcrypt on update

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -51,32 +51,3 @@ module.exports = (sequelize, DataTypes) => {
   Message.sync();
   return Message;
 }
-
-/* 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Message extends Model {
-
-    static associate(models) {
-      models.Message.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      })
-    }
-  };
-  Message.init({
-    USERS_id: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    article: DataTypes.STRING,
-    attachment: DataTypes.STRING,
-    user_like: DataTypes.INTEGER,
-    likes: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Message',
-  });
-  return Message;
-}; */
